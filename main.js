0c1f18b05dbf2845391a371e8cb6f1aec09869cf
Pablo Novaes <pablo.novaes@estudante.ufjf.br>
2023-11-28 23:12:21 -0300

    estoque criado e main criado tambem

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const quantidade =document.entrada.quantidade.value;
-const fruta =document.entrada.fruta.value;
+const olJoao=document.querySelector("#joao");
+const olMaria=document.querySelector("#maria");
+document.entrada.enviar.addEventListener('submit' , leFormulario);
+
+function leFormulario(event){
+    event.preventDefault();
+    const quantidade =document.entrada.quantidade.valueAsNumber;
+    const fruta =document.entrada.fruta.value;
+    const origem=document=document.entrada.origem.value;
+    const destino=document=document.entrada.destino.value;
+}
